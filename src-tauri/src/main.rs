a82e48cc8ded68cd5a116cf0a99837791c607bb0
Leon Si <leonzalion@gmail.com>
2021-06-25 23:55:54 -0400

    state changes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,9 @@
   windows_subsystem = "windows"
 )]
 
+use std::collections::HashMap;
 use std::sync::RwLock;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 use tauri::api::process::{Command, CommandChild, CommandEvent};
 
@@ -13,17 +15,50 @@ struct KaptState {
   // Used to prevent a thread from continuing a chunk recording when the
   // associated recording session has ended
   pub recording_session_id: Option<String>,
+
+  // A map from a session ID to a Vec of FfmpegRecordings
+  pub session_recordings: HashMap<String, Vec<FfmpegRecording>>,
 }
 
 impl KaptState {
   pub fn is_recording(&self) -> bool {
     self.active_recordings[0].is_some() || self.active_recordings[1].is_some()
   }
+
+  pub fn add_recording(&mut self, session_id: String, recording: FfmpegRecording) {
+    // Create a new HashMap entry if the session ID doesn't exist yet
+    if !self.session_recordings.contains_key(&session_id) {
+      self.session_recordings.insert(session_id.clone(), vec![]);
+    }
+
+    self
+      .session_recordings
+      .get_mut(&session_id)
+      .expect("Session recording key not found")
+      .push(recording);
+  }
+
+  pub fn new() -> Self {
+    Self {
+      active_recordings: [None, None],
+      recording_session_id: None,
+      session_recordings: HashMap::new(),
+    }
+  }
 }
 
+// A recording that's currently in process
 struct FfmpegActiveRecording {
   pub path: String,
+  pub start_time: Option<u128>,
   pub command_child: CommandChild,
+}
+
+// A recording that has already ended
+struct FfmpegRecording {
+  pub path: String,
+  pub start_time: u128,
+  pub end_time: u128,
 }
 
 use nanoid::nanoid;
@@ -33,13 +68,39 @@ use std::path::Path;
 fn stop_recording_chunk(state_lock: &'static RwLock<KaptState>, recording_index: usize) {
   let mut state = state_lock.write().expect("Failed to acquire write lock");
 
-  if let Some(mut recording_child) = Option::take(&mut state.active_recordings[recording_index]) {
-    recording_child
-      .command_child
+  let active_recording = Option::take(&mut state.active_recordings[recording_index]);
+  if let Some(active_recording) = active_recording {
+    let FfmpegActiveRecording {
+      start_time,
+      mut command_child,
+      path,
+    } = active_recording;
+
+    let end_time = SystemTime::now()
+      .duration_since(UNIX_EPOCH)
+      .expect("Time went backwards")
+      .as_millis();
+
+    let recording_session_id = state
+      .recording_session_id
+      .clone()
+      .expect("Session ID not present");
+    // Adding a past recording
+    state.add_recording(
+      recording_session_id.clone(),
+      FfmpegRecording {
+        end_time,
+        path: path.clone(),
+        start_time: start_time.expect("Start time not present"),
+      },
+    );
+
+    // Stopping the ffmpeg recording process
+    command_child
       .write(&[b'q'])
       .expect("Failed to stop ffmpeg process");
 
-    println!("Recording 0 path: {}", recording_child.path);
+    println!("Recording {} path: {}", recording_index, path);
   }
 }
 
@@ -84,23 +145,50 @@ fn start_recording_chunk(
   let mut state = state_lock
     .write()
     .expect("Failed to acquire state write lock");
-  state.active_recordings[0] = Some(FfmpegActiveRecording {
+  state.active_recordings[recording_index] = Some(FfmpegActiveRecording {
     command_child,
     path,
+    start_time: None,
   });
 
   let window_clone = window.clone();
   // Spawn a recording chunk for right now
   tauri::async_runtime::spawn(async move {
+    println!("here");
     let window = window_clone;
     // read events such as stdout
     while let Some(event) = rx.recv().await {
       println!("{:?}", event);
-      if let CommandEvent::Stdout(line) = event {
+      // Ffmpeg logs to stderr
+      if let CommandEvent::Stderr(line) = event {
+        use regex::Regex;
+        lazy_static! {
+          static ref START_TIME_RE: Regex =
+            Regex::new(r#"start: (\d+),"#).expect("Failed to compile regex");
+        };
+
         println!("{}", line);
-        window
-          .emit("message", Some(format!("'{}'", line)))
-          .expect("failed to emit event");
+        if let Some(cap) = START_TIME_RE.captures(&line) {
+          println!("captured: {}", line);
+          if let Some(m) = cap.get(1) {
+            let mut state = state_lock
+              .write()
+              .expect("Failed to acquire state read lock");
+            let unix_timestamp = m
+              .as_str()
+              .to_string()
+              .parse::<u128>()
+              .expect("Failed to parse integer");
+            if let Some(ref mut recording) = &mut state.active_recordings[recording_index] {
+              recording.start_time = Some(unix_timestamp)
+            }
+          }
+
+          println!("{}", line);
+          window
+            .emit("message", Some(format!("'{}'", line)))
+            .expect("failed to emit event");
+        }
       }
     }
   });
@@ -158,10 +246,7 @@ fn stop_recording(state_lock: tauri::State<&'static RwLock<KaptState>>) {
 
 use lazy_static::lazy_static;
 lazy_static! {
-  static ref KAPT_STATE: RwLock<KaptState> = RwLock::new(KaptState {
-    recording_session_id: None,
-    active_recordings: [None, None],
-  });
+  static ref KAPT_STATE: RwLock<KaptState> = RwLock::new(KaptState::new());
 }
 
 fn main() {
